de3500eb066fbe3b74006f25f854993311b8a007
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-02-09 20:07:01 +0600

    add user management routes for adding and removing users with validation

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -1,12 +1,30 @@
 // External Imports
 const express = require("express");
+const { check, validationResult } = require("express-validator");
 
 // Internal Imports
-const { getUsers } = require("../controller/usersController");
+const { getUsers, addUser, removeUser } = require("../controller/usersController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const avatarUpload = require("../middlewares/users/avatarUpload");
+const {
+  addUserValidators,
+  addUserValidationHandler,
+} = require("../middlewares/users/userValidators");
 const router = express.Router();
 
 // Login page
 router.get("/", decorateHtmlResponse("Users"), getUsers);
+
+// add user
+router.post(
+  "/",
+  avatarUpload,
+  addUserValidators,
+  addUserValidationHandler,
+  addUser
+);
+
+// remove user
+router.delete("/:id", removeUser);
 
 module.exports = router;
