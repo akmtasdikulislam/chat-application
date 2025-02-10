cc6b7d8035c86ff007f9f294dd8e332960d76956
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-02-10 23:48:51 +0600

    add login validation middleware for username and password fields

---
+++ middlewares/login/loginValidators.js
@@ -0,0 +1,27 @@
+const { check, validationResult } = require("express-validator");
+
+const doLoginValidators = [
+  check("username").isLength({ min: 1 }).withMessage("Username is required"),
+  check("password").isLength({ min: 1 }).withMessage("Password is required"),
+];
+
+const doLoginValidationHandler = function (req, res, next) {
+  const errors = validationResult(req);
+  const mappedErrors = errors.mapped();
+  if (Object.keys(mappedErrors).length === 0) {
+    next();
+  } else {
+    // response the errors
+    res.render("index", {
+      data: {
+        username: req.body.username,
+      },
+      errors: mappedErrors,
+    });
+  }
+};
+
+module.exports = {
+  doLoginValidators,
+  doLoginValidationHandler,
+};
