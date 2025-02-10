5809645f0e57aed70b4ca90cbb1b7a51b63897e0
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-02-10 23:50:11 +0600

    add login check middleware to inbox route for access control

--- router/inboxRouter.js
+++ router/inboxRouter.js
@@ -4,9 +4,10 @@ const express = require("express");
 // Internal Imports
 const { getInbox } = require("../controller/inboxController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const {checkLogin} = require("../middlewares/common/checkLogin");
 const router = express.Router();
 
 // Login page
-router.get("/",decorateHtmlResponse("Inbox"), getInbox);
+router.get("/",decorateHtmlResponse("Inbox"), checkLogin, getInbox);
 
 module.exports = router;
