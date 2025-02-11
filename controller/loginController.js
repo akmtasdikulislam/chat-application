278285efb980acda9af45664f574d8704b96d1f4
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-02-11 18:29:25 +0600

    redirect to inbox after successful login instead of rendering

--- controller/loginController.js
+++ controller/loginController.js
@@ -49,7 +49,7 @@ async function login(req, res, next) {
         // set logged in user local identifier
         res.locals.loggedInUser = userObject;
 
-        res.render("inbox");
+        res.redirect("inbox");
       } else {
         throw createError("Login failed! Please try again later.");
       }
