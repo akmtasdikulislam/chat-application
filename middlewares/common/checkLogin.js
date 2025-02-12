9c4c90b9ffb1ec52f163c3ee6e44a60708cf407d
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-02-12 18:13:40 +0600

    feat: add role-based authorization middleware for route protection

--- middlewares/common/checkLogin.js
+++ middlewares/common/checkLogin.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const createError = require("http-errors");
 
 const checkLogin = (req, res, next) => {
   const cookies =
@@ -55,4 +56,25 @@ const redirectLoggedIn = function (req, res, next) {
   }
 };
 
-module.exports = { checkLogin, redirectLoggedIn };
+// guard to protect routes that need role based authorization
+function requireRole(role) {
+  return function (req, res, next) {
+    if (req.user.role && role.includes(req.user.role)) {
+      next();
+    } else {
+      if (res.locals.html) {
+        next(createError(401, "You are not authorized to access this page!"));
+      } else {
+        res.status(401).json({
+          errors: {
+            common: {
+              msg: "You are not authorized!",
+            },
+          },
+        });
+      }
+    }
+  };
+}
+
+module.exports = { checkLogin, redirectLoggedIn, requireRole };
